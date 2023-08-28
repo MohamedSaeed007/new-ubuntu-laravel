8c7527770fd7fd54376b181fa0ea45457779aebe
Mohamed Saeed <57630471+MohamedSaeed007@users.noreply.github.com>
2023-08-28 12:45:32 +0300

    Update script7.4.sh

--- script7.4.sh
+++ script7.4.sh
@@ -110,7 +110,7 @@ sudo bash -c "echo 'server {
     }
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/var/run/php/php8.2-fpm.sock;
+        fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
     }
     location ~ /\.ht {
             deny all;
